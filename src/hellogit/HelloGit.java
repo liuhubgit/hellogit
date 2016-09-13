436ad5b7bc2358252c0da7ca60a52aa03b8697b9
liuhubgit <chengjinliu@126.com>
2016-09-13 11:51:37 +0800

    init version 1.0

---
+++ src/hellogit/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package hellogit;
+
+/**
+ * @author liujc
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git");
+	}
+
+}
